50f5f145d1f0482881e1ff3939af970f39ac35d2
yoshiki1214 <gazerk43@gmail.com>
2025-09-24 22:32:08 +0900

    課題B修正

--- beautiful_kuku.py
+++ beautiful_kuku.py
@@ -4,5 +4,5 @@ col_count = int(input("列数を入力してください: "))
 for row in range(1, row_count + 1):  # 1から指定行数まで
     for col in range(1, col_count + 1):  # 1から指定列数まで
         result = row * col
-        print(f"{row} ✕ {col} = {result:2d}", end=" |")  # 行と列を掛けて出力し、スペースで区切る
+        print(f"{col} ✕ {row} = {result:2d}", end=" | ")  # 行と列を掛けて出力し、スペースで区切る
     print()
